a7bd8f88701bc3e557f0516c187620f6d5bc3b45
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-11-15 13:32:37 -0700

    Converts an integer into an ASCII code.

---
+++ src/AlannaBotscharowChapter5Program1/Chapter5Program1.java
@@ -0,0 +1,417 @@
+package AlannaBotscharowChapter5Program1;
+import java.util.Scanner;
+/*Alanna Botscharow 
+ * 10/18
+ * ASCII code characters
+ */
+public class Chapter5Program1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// user input of a letter between 0 and 127.
+			System.out.println("enter an integer between 0 and 127: ");
+		
+				int ch = input.nextInt(); 
+			
+				
+				
+				
+				
+			
+			
+			
+			
+			
+		//Display its character. 
+				
+				if (ch == 0){
+					System.out.println("The character for ASCII code " + ch + " is NUL"  );
+				}
+				else if (ch == 1){
+					System.out.println("The character for ASCII code " + ch + " is SOH"  );
+				}
+				else if (ch == 2){
+					System.out.println("The character for ASCII code " + ch + " is STX"  );
+				}
+				else if (ch == 3){
+					System.out.println("The character for ASCII code " + ch + " is ETX"  );
+				}
+				else if (ch == 4){
+					System.out.println("The character for ASCII code " + ch + " is EOT"  );
+				}
+				else if (ch == 5){
+					System.out.println("The character for ASCII code " + ch + " is ENQ"  );
+				}
+				else if (ch == 6){
+					System.out.println("The character for ASCII code " + ch + " is ACK"  );
+				}
+				else if (ch == 7){
+					System.out.println("The character for ASCII code " + ch + " is BEL"  );
+				}
+				else if (ch == 8){
+					System.out.println("The character for ASCII code " + ch + " is BS"  );
+				}
+				else if (ch == 9){
+					System.out.println("The character for ASCII code " + ch + " is TAB"  );
+				}
+				else  if (ch == 10){
+					System.out.println("The character for ASCII code " + ch + " is LF"  );
+				}
+				else  if (ch == 11){
+					System.out.println("The character for ASCII code " + ch + " is VT"  );
+				}
+				else  if (ch == 12){
+					System.out.println("The character for ASCII code " + ch + " is FF"  );
+				}
+				else  if (ch == 13){
+					System.out.println("The character for ASCII code " + ch + " is CR"  );
+				}
+				else  if (ch == 14){
+					System.out.println("The character for ASCII code " + ch + " is SO"  );
+				}
+				else  if (ch == 15){
+					System.out.println("The character for ASCII code " + ch + " is SI"  );
+				}
+				else  if (ch == 16){
+					System.out.println("The character for ASCII code " + ch + " is DLE"  );
+				}
+				else  if (ch == 17){
+					System.out.println("The character for ASCII code " + ch + " is DC1"  );
+				}
+				else  if (ch == 18){
+					System.out.println("The character for ASCII code " + ch + " is DC2"  );
+				}
+				else  if (ch == 19){
+					System.out.println("The character for ASCII code " + ch + " is DC3"  );
+				}
+				else  if (ch == 20){
+					System.out.println("The character for ASCII code " + ch + " is DC4"  );
+				}
+				else  if (ch == 21){
+					System.out.println("The character for ASCII code " + ch + " is NAK"  );
+				}
+				else  if (ch == 22){
+					System.out.println("The character for ASCII code " + ch + " is SYN"  );
+				}
+				else  if (ch == 23){
+					System.out.println("The character for ASCII code " + ch + " is ETB"  );
+				}
+				else  if (ch == 24){
+					System.out.println("The character for ASCII code " + ch + " is CAN"  );
+				}
+				else  if (ch == 25){
+					System.out.println("The character for ASCII code " + ch + " is EM"  );
+				}
+				else  if (ch == 26){
+					System.out.println("The character for ASCII code " + ch + " is SUB"  );
+				}
+				else  if (ch == 27){
+					System.out.println("The character for ASCII code " + ch + " is ESC"  );
+				}
+				else  if (ch == 28){
+					System.out.println("The character for ASCII code " + ch + " is FS"  );
+				}
+				else  if (ch == 29){
+					System.out.println("The character for ASCII code " + ch + " is GS"  );
+				}
+				else  if (ch == 30){
+					System.out.println("The character for ASCII code " + ch + " is RS"  );
+				}
+				else  if (ch == 31){
+					System.out.println("The character for ASCII code " + ch + " is US"  );
+				}
+				else  if (ch == 32){
+					System.out.println("The character for ASCII code " + ch + " is Space"  );
+				}
+				else  if (ch == 33){
+					System.out.println("The character for ASCII code " + ch + " is !"  );
+				}
+				else  if (ch == 34){
+					System.out.println("The character for ASCII code " + ch + " is " +"\""   );
+				}
+				else  if (ch == 35){
+					System.out.println("The character for ASCII code " + ch + " is #" );
+				}
+				else  if (ch == 36){
+					System.out.println("The character for ASCII code " + ch + " is $" );
+				}
+				else  if (ch == 37){
+					System.out.println("The character for ASCII code " + ch + " is %" );
+				}
+				else  if (ch == 38){
+					System.out.println("The character for ASCII code " + ch + " is &" );
+				}
+				else  if (ch == 39){
+					System.out.println("The character for ASCII code " + ch + " is '" );
+				}
+				else  if (ch == 40){
+					System.out.println("The character for ASCII code " + ch + " is (" );
+				}
+				else  if (ch == 41){
+					System.out.println("The character for ASCII code " + ch + " is )" );
+				}
+				else  if (ch == 42){
+					System.out.println("The character for ASCII code " + ch + " is *" );
+				}
+				else  if (ch == 43){
+					System.out.println("The character for ASCII code " + ch + " is +" );
+				}
+				else  if (ch == 44){
+					System.out.println("The character for ASCII code " + ch + " is ," );
+				}
+				else  if (ch == 45){
+					System.out.println("The character for ASCII code " + ch + " is -" );
+				}
+				else  if (ch == 46){
+					System.out.println("The character for ASCII code " + ch + " is ." );
+				}
+				else  if (ch == 47){
+					System.out.println("The character for ASCII code " + ch + " is /" );
+				}
+				else  if (ch == 48){
+					System.out.println("The character for ASCII code " + ch + " is 0" );
+				}
+				else  if (ch == 49){
+					System.out.println("The character for ASCII code " + ch + " is 1" );
+				}
+				else  if (ch == 50){
+					System.out.println("The character for ASCII code " + ch + " is 2" );
+				}
+				else  if (ch == 51){
+					System.out.println("The character for ASCII code " + ch + " is 3" );
+				}
+				else  if (ch == 52){
+					System.out.println("The character for ASCII code " + ch + " is 4" );
+				}
+				else  if (ch == 53){
+					System.out.println("The character for ASCII code " + ch + " is 5" );
+				}
+				else  if (ch == 54){
+					System.out.println("The character for ASCII code " + ch + " is 6" );
+				}
+				else  if (ch == 55){
+					System.out.println("The character for ASCII code " + ch + " is 7" );
+				}
+				else  if (ch == 56){
+					System.out.println("The character for ASCII code " + ch + " is 8" );
+				}
+				else  if (ch == 57){
+					System.out.println("The character for ASCII code " + ch + " is 9" );
+				}
+				else  if (ch == 58){
+					System.out.println("The character for ASCII code " + ch + " is :" );
+				}
+				else  if (ch == 59){
+					System.out.println("The character for ASCII code " + ch + " is ;" );
+				}
+				else  if (ch == 60){
+					System.out.println("The character for ASCII code " + ch + " is <" );
+				}
+				else  if (ch == 61){
+					System.out.println("The character for ASCII code " + ch + " is =" );
+				}
+				else  if (ch == 62){
+					System.out.println("The character for ASCII code " + ch + " is >" );
+				}
+				else  if (ch == 63){
+					System.out.println("The character for ASCII code " + ch + " is ?" );
+				}
+				else  if (ch == 64){
+					System.out.println("The character for ASCII code " + ch + " is @" );
+				}
+				else  if (ch == 65){
+					System.out.println("The character for ASCII code " + ch + " is A" );
+				}
+				else  if (ch == 66){
+					System.out.println("The character for ASCII code " + ch + " is B" );
+				}
+				else  if (ch == 67){
+					System.out.println("The character for ASCII code " + ch + " is C" );
+				}
+				else  if (ch == 68){
+					System.out.println("The character for ASCII code " + ch + " is D" );
+				}
+				else  if (ch == 69){
+					System.out.println("The character for ASCII code " + ch + " is E" );
+				}
+				else  if (ch == 70){
+					System.out.println("The character for ASCII code " + ch + " is F" );
+				}
+				else  if (ch == 71){
+					System.out.println("The character for ASCII code " + ch + " is G" );
+				}
+				else  if (ch == 72){
+					System.out.println("The character for ASCII code " + ch + " is H" );
+				}
+				else  if (ch == 73){
+					System.out.println("The character for ASCII code " + ch + " is I" );
+				}
+				else  if (ch == 74){
+					System.out.println("The character for ASCII code " + ch + " is J" );
+				}
+				else  if (ch == 75){
+					System.out.println("The character for ASCII code " + ch + " is K" );
+				}
+				else  if (ch == 76){
+					System.out.println("The character for ASCII code " + ch + " is L" );
+				}
+				else  if (ch == 77){
+					System.out.println("The character for ASCII code " + ch + " is M" );
+				}
+				else  if (ch == 78){
+					System.out.println("The character for ASCII code " + ch + " is N" );
+				}
+				else  if (ch == 79){
+					System.out.println("The character for ASCII code " + ch + " is O" );
+				}
+				else  if (ch == 80){
+					System.out.println("The character for ASCII code " + ch + " is P" );
+				}
+				else  if (ch == 81){
+					System.out.println("The character for ASCII code " + ch + " is Q" );
+				}
+				else  if (ch == 82){
+					System.out.println("The character for ASCII code " + ch + " is R" );
+				}
+				else  if (ch == 83){
+					System.out.println("The character for ASCII code " + ch + " is S" );
+				}
+				else  if (ch == 84){
+					System.out.println("The character for ASCII code " + ch + " is T" );
+				}
+				else  if (ch == 85){
+					System.out.println("The character for ASCII code " + ch + " is U" );
+				}
+				else  if (ch == 86){
+					System.out.println("The character for ASCII code " + ch + " is V" );
+				}
+				else  if (ch == 87){
+					System.out.println("The character for ASCII code " + ch + " is W" );
+				}
+				else  if (ch == 88){
+					System.out.println("The character for ASCII code " + ch + " is X" );
+				}
+				else  if (ch == 89){
+					System.out.println("The character for ASCII code " + ch + " is Y" );
+				}
+				else  if (ch == 90){
+					System.out.println("The character for ASCII code " + ch + " is Z" );
+				}
+				else  if (ch == 91){
+					System.out.println("The character for ASCII code " + ch + " is [" );
+				}
+				else  if (ch == 92){
+					System.out.println("The character for ASCII code " + ch + " is \\" );
+				}
+				else  if (ch == 93){
+					System.out.println("The character for ASCII code " + ch + " is ]" );
+				}
+				else  if (ch == 94){
+					System.out.println("The character for ASCII code " + ch + " is ^" );
+				}
+				else  if (ch == 95){
+					System.out.println("The character for ASCII code " + ch + " is _" );
+				}
+				else  if (ch == 96){
+					System.out.println("The character for ASCII code " + ch + " is `" );
+				}
+				else  if (ch == 97){
+					System.out.println("The character for ASCII code " + ch + " is a" );
+				}
+				else  if (ch == 98){
+					System.out.println("The character for ASCII code " + ch + " is b" );
+				}
+				else  if (ch == 99){
+					System.out.println("The character for ASCII code " + ch + " is c" );
+				}
+				else  if (ch == 100){
+					System.out.println("The character for ASCII code " + ch + " is d" );
+				}
+				else  if (ch == 101){
+					System.out.println("The character for ASCII code " + ch + " is e" );
+				}
+				else  if (ch == 102){
+					System.out.println("The character for ASCII code " + ch + " is f" );
+				}
+				else  if (ch == 103){
+					System.out.println("The character for ASCII code " + ch + " is g" );
+				}
+				else  if (ch == 104){
+					System.out.println("The character for ASCII code " + ch + " is h" );
+				}
+				else  if (ch == 105){
+					System.out.println("The character for ASCII code " + ch + " is i" );
+				}
+				else  if (ch == 106){
+					System.out.println("The character for ASCII code " + ch + " is j" );
+				}
+				else  if (ch == 107){
+					System.out.println("The character for ASCII code " + ch + " is k" );
+				}
+				else  if (ch == 108){
+					System.out.println("The character for ASCII code " + ch + " is l" );
+				}
+				else  if (ch == 109){
+					System.out.println("The character for ASCII code " + ch + " is m" );
+				}
+				else  if (ch == 110){
+					System.out.println("The character for ASCII code " + ch + " is n" );
+				}
+				else  if (ch == 111){
+					System.out.println("The character for ASCII code " + ch + " is o" );
+				}
+				else  if (ch == 112){
+					System.out.println("The character for ASCII code " + ch + " is p" );
+				}
+				else  if (ch == 113){
+					System.out.println("The character for ASCII code " + ch + " is q" );
+				}
+				else  if (ch == 114){
+					System.out.println("The character for ASCII code " + ch + " is r" );
+				}
+				else  if (ch == 115){
+					System.out.println("The character for ASCII code " + ch + " is s" );
+				}
+				else  if (ch == 116){
+					System.out.println("The character for ASCII code " + ch + " is t" );
+				}
+				else  if (ch == 117){
+					System.out.println("The character for ASCII code " + ch + " is u" );
+				}
+				else  if (ch == 118){
+					System.out.println("The character for ASCII code " + ch + " is v" );
+				}
+				else  if (ch == 119){
+					System.out.println("The character for ASCII code " + ch + " is w" );
+				}
+				else  if (ch == 120){
+					System.out.println("The character for ASCII code " + ch + " is x" );
+				}
+				else  if (ch == 121){
+					System.out.println("The character for ASCII code " + ch + " is y" );
+				}
+				else  if (ch == 122){
+					System.out.println("The character for ASCII code " + ch + " is z" );
+				}
+				else  if (ch == 123){
+					System.out.println("The character for ASCII code " + ch + " is {" );
+				}
+				else  if (ch == 124){
+					System.out.println("The character for ASCII code " + ch + " is |" );
+				}
+				else  if (ch == 125){
+					System.out.println("The character for ASCII code " + ch + " is }" );
+				}
+				else  if (ch == 126){
+					System.out.println("The character for ASCII code " + ch + " is ~" );
+				}
+				else  if (ch == 127){
+					System.out.println("The character for ASCII code " + ch + " is DEL" );
+				}
+				
+				
+				
+	}
+
+}
